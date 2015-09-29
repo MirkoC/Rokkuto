9577a055788417e45d0ac9057ad455cc52e3a171
Mirko Cerovac <mirko.cerovac@gmail.com>
2015-09-29 13:18:02 +0200

    Added users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928134518) do
+ActiveRecord::Schema.define(version: 20150929111727) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'applications', force: :cascade do |t|
-    t.string   'name'
-    t.string   'api_key'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "applications", force: :cascade do |t|
+    t.string   "name"
+    t.string   "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
